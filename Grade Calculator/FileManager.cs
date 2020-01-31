ffe16636d0250a521bc67d0c0a36ed9346d4b2b0
Daniel Jeselnik <dannyjeselnik@gmail.com>
2020-01-31 01:54:30 -0500

    Added title. Stubbing out file IO Closes #3 Committing before swapping to Laptop

---
+++ Grade Calculator/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Serialization;
+
+namespace Grade_Calculator
+{
+    /// <summary>
+    ///     Manages loading and saving files using XML Serialization
+    /// </summary>
+    public class FileManager
+    {
+        #region Methods
+
+        /// <summary>
+        ///     Loads the grade entries.
+        /// </summary>
+        public void LoadGradeEntries()
+        {
+            //TODO
+        }
+
+        /// <summary>
+        ///     Saves the grade entries.
+        /// </summary>
+        public void SaveGradeEntries()
+        {
+            //var serializer = XmlSerializer();
+            //TODO
+        }
+
+        #endregion
+    }
+}
